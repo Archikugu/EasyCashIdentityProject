f18d8b64668ad82598c2731f438ecfe8979f2135
Gokhan <engokhangok@gmail.com>
2023-05-01 18:15:09 +0300

    Created Customer Account & Customer Account Process.

---
+++ Entities/Concrete/CustomerAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class CustomerAccount
+    {
+        public int CustomerAccountID { get; set; }
+        public string CustomerAccountNumber { get; set; }
+        public string CustomerAccountCurrency { get; set; }
+        public decimal CustomerAccountBalance { get; set; }
+        public string BankBranch { get; set; }
+
+    }
+}
