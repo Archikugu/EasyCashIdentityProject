6e0b9027ca5d51a168d51e7a54ebba785883e945
Gokhan <engokhangok@gmail.com>
2023-05-02 14:46:04 +0300

    Added identity library and added to database

---
+++ DataAccess/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class Context : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-4K38GM2;initial catalog=EasyCashDb;integrated Security = true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
