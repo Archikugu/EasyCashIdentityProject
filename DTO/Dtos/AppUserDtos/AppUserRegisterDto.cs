f9f6a09265586089e3f38b2c15c797eba6cc8a23
Gokhan <engokhangok@gmail.com>
2023-05-05 12:54:04 +0300

    Added Dto layer & AppUserRegisterDto Class

---
+++ DTO/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Dtos.AppUserDtos
+{
+    public class AppUserRegisterDto
+    {
+        //[Required(ErrorMessage = "Name field is required")]
+        //[Display(Name = "Name")]
+        //[MaxLength(30, ErrorMessage = "Name field must be a maximum of 30 characters")]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
