e47bd18dd4464446af0d63c2ca15ec22b59416b7
Gokhan <engokhangok@gmail.com>
2023-05-08 17:59:29 +0300

    Added IGenericDal & GenericRepository

---
+++ DataAccess/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+        T GetByID(int id);
+        List<T> GetList();
+        List<T> GetListByFilter(Expression<Func<T, bool>> filter);
+    }
+}
---
+++ DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+        }
+
+        public List<T> GetListByFilter(Expression<Func<T, bool>> filter)
+        {
+            using var c = new Context();
+            return c.Set<T>().Where(filter).ToList();
+        }
+
+        public void Insert(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Update(t);
+            context.SaveChanges();
+        }
+    }
+}
