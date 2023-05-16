3ea0f6a68a4492da52d9723c4649377f584746d8
Gokhan <engokhangok@gmail.com>
2023-05-16 22:52:33 +0300

    Confirm code created and added.

--- EasyCash.UI/Controllers/RegisterController.cs
+++ EasyCash.UI/Controllers/RegisterController.cs
@@ -23,6 +23,8 @@ namespace EasyCash.UI.Controllers
         {
             if (ModelState.IsValid)
             {
+                Random random = new Random();
+
                 AppUser appUser = new AppUser()
                 {
                     UserName = appUserRegisterDto.UserName,
@@ -31,25 +33,27 @@ namespace EasyCash.UI.Controllers
                     Email = appUserRegisterDto.Email,
                     City = appUserRegisterDto.City,
                     District = "Yenimahalle",
-                    ImageUrl ="-"
+                    ImageUrl = "-",
+                    ConfirmCode = random.Next(100000, 1000000)
 
-                    
-                };
-                var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
-                if (result.Succeeded)
+
+
+            };
+            var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "ConfirmMail");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
                 {
-                    return RedirectToAction("Index", "ConfirmMail");
-                }
-                else
-                {
-                    foreach (var item in result.Errors)
-                    {
-                        ModelState.AddModelError("", item.Description);
-                    }
+                    ModelState.AddModelError("", item.Description);
                 }
             }
+        }
 
             return View();
-        }
     }
+}
 }
