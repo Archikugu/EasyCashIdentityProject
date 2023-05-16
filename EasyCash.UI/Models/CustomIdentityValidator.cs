b51934572fbdd46cd0d92c1c96aa8369ecb83dd4
Gokhan <engokhangok@gmail.com>
2023-05-16 22:30:51 +0300

    Added Error Describer

---
+++ EasyCash.UI/Models/CustomIdentityValidator.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EasyCash.UI.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"The password must contain at least {length} characters"
+            };
+        }
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Your password must contain at least 1 upper case"
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Your password must contain at least 1 lower case"
+            };
+        }
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresDigit",
+                Description = "Your password must contain at least 1 digit"
+            };
+        }
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Your password must contain at least 1 symbol"
+            };
+        }
+
+    }
+}
